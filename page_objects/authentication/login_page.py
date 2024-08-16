8c51ed232c8416287c7b67851f4fb25e36780405
Alex Saunders <alex.saunders@wildlifeprotectionsolutions.org>
2024-08-15 20:46:08 -0600

    changed login_page.py, updated variable strings, changed logger calls

--- page_objects/authentication/login_page.py
+++ page_objects/authentication/login_page.py
@@ -1,23 +1,23 @@
 # login_page.py
 import os
 import time
-from page_objects import BasePage
+from page_objects.common.base_page import BasePage
 from dotenv import load_dotenv
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from utilities.config import DEFAULT_TIMEOUT, SCREENSHOT_DIR
-from utilities import CustomLogger
-from utilities import ElementInteractor
-from utilities import ElementLocator
-from utilities import ScreenshotManager
+from utilities.utils import logger
+from utilities.element_interactor import ElementInteractor
+from utilities.element_locator import ElementLocator
+from utilities.screenshot_manager import ScreenshotManager
 
 load_dotenv()
 
 # Environmental Variables
 
-BASE_URL = os.getenv('QA_BASE_URL')
+BASE_URL = os.getenv("QA_BASE_URL")
 
 class LoginPage(BasePage):
     """_summary_
@@ -29,30 +29,30 @@ class LoginPage(BasePage):
         self.locator = ElementLocator(driver)
         self.interactor = ElementInteractor(driver)
         self.screenshot = ScreenshotManager()
-        self.logger = CustomLogger()
+        self.logger = logger
         
     class ElementLocators:
-        LOGIN_LINK = ("//section//button[text()='LOG IN']", "xpath")
-        USERNAME_INPUT = ("//div//input[@name='Email']", "xpath")
-        PASSWORD_INPUT = ("//div//input[@name='Password']", "xpath")
-        LOGIN_BUTTON = ("//div//button[@id='SubmitButton']", "xpath")
-        
+        LOGIN_LINK = "//section//button[text()='LOG IN']"
+        USERNAME_INPUT = "//div//input[@name='Email']"
+        PASSWORD_INPUT = "//div//input[@name='Password']"
+        LOGIN_BUTTON = "//div//button[@id='SubmitButton']"
+        LOGOUT_BUTTON = "//section//button[text()='LOG OUT']"
     
     class ErrorMessages:
-        BASE_XPATH = "//div[contains(@class, 'alert-danger)]"
-        ERROR_BLANK_INPUT = (f"{BASE_XPATH}[contains(., 'Password field is required') and contains(., 'UserName field is required')]", "xpath")
-        ERROR_NO_PASS = (f"{BASE_XPATH}[contains(., 'Password field is required')]", "xpath")
-        ERROR_NO_USER =  (f"{BASE_XPATH}[contains(., 'UserName field is required')]", "xpath")
-        ERROR_INVALID = (f"{BASE_XPATH}[contains(.,'Failed')]", "xpath")
-        ERROR_PASS_TOO_SHORT = (f"{BASE_XPATH}[contains(.(), 'Password must be a string or array type with a minimum length of '4')]", "xpath")
-        ERROR_USER_TOO_SHORT = (f"{BASE_XPATH}[contains(.(), 'UserName must be a string or array type with a minimum length of '4')]", "xpath")
+        BASE_XPATH = "//div[contains(@class, 'alert-danger) and"
+        ERROR_BLANK_INPUT = "//div[contains(@class, 'alert-danger') and contains(., 'UserName field is required') and contains(., 'Password field is required')]"
+        ERROR_NO_PASS = "//div[contains(@class, 'alert-danger) and contains(., 'Password field is required')]"
+        ERROR_NO_USER =  "//div[contains(@class, 'alert-danger) and contains(., 'UserName field is required')]"
+        ERROR_INVALID = "//div[contains(@class, 'alert-danger) and contains(.,'Failed')]"
+        ERROR_PASS_TOO_SHORT = "//div[contains(@class, 'alert-danger) and contains(.(), 'Password must be a string or array type with a minimum length of '4')]"
+        ERROR_USER_TOO_SHORT = "//div[contains(@class, 'alert-danger) and contains(.(), 'UserName must be a string or array type with a minimum length of '4')]"
 
     
     def click_login_link(self):
         """_summary_
         """
         self.logger.info('Clicking login link')
-        self.interactor.element_click(self._login_link)
+        self.interactor.element_click(self.ElementLocators.LOGIN_LINK)
         
     def enter_username(self, user):
         """_summary_
@@ -60,7 +60,7 @@ class LoginPage(BasePage):
         Args:
             user (_type_): _description_
         """
-        self.logger.info("Entering user: {user}")
+        self.logger.info("Entering user.")
         self.interactor.clear_element_input(self.ElementLocators.USERNAME_INPUT)
         self.interactor.element_send_input(user, self.ElementLocators.USERNAME_INPUT)
     
@@ -80,7 +80,7 @@ class LoginPage(BasePage):
         self.logger.info("Clicking the Log In button")
         self.interactor.element_click(self.ElementLocators.LOGIN_BUTTON)
         
-    def verify_all_elements_present(self):
+    def verify_all_elements_present(self) -> bool:
         """_summary_
 
         Returns:
@@ -88,11 +88,11 @@ class LoginPage(BasePage):
         """
         self.logger.info("Verifying that all expected elements are present")
         try:
-            for page_element in [*self.CommonLocators.HEADER_LOGO,
-                            *self.CommonLocators.LOGIN_LINK,
-                            *self.ElementLocators.PASSWORD_INPUT,
-                            *self.ElementLocators.USERNAME_INPUT,
-                            *self.ElementLocators.LOGIN_BUTTON
+            for page_element in [self.CommonLocators.HEADER_LOGO,
+                            self.CommonLocators.LOGIN_LINK,
+                            self.ElementLocators.PASSWORD_INPUT,
+                            self.ElementLocators.USERNAME_INPUT,
+                            self.ElementLocators.LOGIN_BUTTON
                             ]:
                 self.locator.check_elements_present(page_element)
                 self.logger.info(f"{page_element} was located successfully.")
@@ -102,72 +102,84 @@ class LoginPage(BasePage):
             return False
         except Exception as e:
             self.logger.error(f"Unexpected error while finding elements: {str(e)}")
+            return False
 
-    def verify_both_missing(self):
+    def verify_both_missing(self) -> bool:
         
         try:
-            self.locator.is_element_present(*self.ErrorMessages.ERROR_BLANK_INPUT)
-            self.screenshot.take_screenshot("No_Creds")
+            self.locator.is_element_present(self.ErrorMessages.ERROR_BLANK_INPUT)
+            self.screenshot.take_screenshot(self.driver, "No_Creds")
             self.logger.info(f"Login was not successful, Missing credentials")
+            return True
         except Exception as e:
-            self.screenshot.take_screenshot("Error_No_Creds")
+            self.screenshot.take_screenshot(self.driver, "Error_No_Creds")
             self.logger.error(f"There was an Error with No Creds check: {str(e)}")
+            return False
 
-    def verify_user_missing(self):
+    def verify_user_missing(self) -> bool:
         """_summary_
         """
         try:
-            self.locator.is_element_present(*self.ErrorMessages.ERROR_NO_USER)
-            self.screenshot.take_screenshot("Missing_User")
+            self.locator.is_element_present(self.ErrorMessages.ERROR_NO_USER)
+            self.screenshot.take_screenshot(self.driver, "Missing_User")
             self.logger.info(f"Login was not successful, Missing Username")
+            return True
         except Exception as e:
-            self.screenshot.take_screenshot("Error_Missing_User")
+            self.screenshot.take_screenshot(self.driver, "Error_Missing_User")
             self.logger.error(f"There was an Error with the User Missing check: {str(e)}")
+            return False
             
-    def verify_password_missing(self):
+    def verify_password_missing(self) -> bool:
         """_summary_
         """
         try:
-            self.locator.is_element_present(*self.ErrorMessages.ERROR_NO_PASS)
-            self.screenshot.take_screenshot("Missing_Pass")
+            self.locator.is_element_present(self.ErrorMessages.ERROR_NO_PASS)
+            self.screenshot.take_screenshot(self.driver, "Missing_Pass")
             self.logger.info(f"Login was not successful, Missing Password")
+            return True
         except Exception as e:
             self.screenshot.take_screenshot("Error_Missing_Password")
             self.logger.error(f"There was an Error with the Password Missing check: {str(e)}")
+            return False
             
-    def verify_user_short(self):
+    def verify_user_short(self) -> bool:
         """_summary_
         """
         try:
-            self.locator.is_element_present(*self.ErrorMessages.ERROR_USER_TOO_SHORT)
-            self.screenshot.take_screenshot("Short_User")
+            self.locator.is_element_present(self.ErrorMessages.ERROR_USER_TOO_SHORT)
+            self.screenshot.take_screenshot(self.driver, "Short_User")
             self.logger.info(f"Login was not successful, Username too short")
+            return True
         except Exception as e:
-            self.screenshot.take_screenshot("Error_User_Short")
+            self.screenshot.take_screenshot(self.driver, "Error_User_Short")
             self.logger.error(f"There was an Error with the User too short check: {str(e)}")
+            return False
     
-    def verify_pass_short(self):
+    def verify_pass_short(self) -> bool:
         """_summary_
         """
         try:
-            self.locator.is_element_present(*self.ErrorMessages.ERROR_PASS_TOO_SHORT)
-            self.screenshot.take_screenshot("Short_Pass")
+            self.locator.is_element_present(self.ErrorMessages.ERROR_PASS_TOO_SHORT)
+            self.screenshot.take_screenshot(self.driver, "Short_Pass")
             self.logger.info(f"Login was not successful, Password too short")
+            return True
         except Exception as e:
-            self.screenshot.take_screenshot("Error_Pass_Short")
+            self.screenshot.take_screenshot(self.driver, "Error_Pass_Short")
             self.logger.error(f"There was an Error with the Password too short check: {str(e)}")
+            return False
     
-    def verify_login_success(self):
+    def verify_login_success(self) -> bool:
         """_summary_
 
         Returns:
             _type_: _description_
         """
         try:
-            expected_url = self.get_current_url()
-            if expected_url == BASE_URL:
-                self.logger.info("Login successful.")
+            element = self.locator.get_element(self.CommonLocators.LOGOUT_BUTTON)
+            if element is not None:
+                logger.info(f"Success in finding element: {element.text}")
                 return True
+            return False
         except TimeoutException:
             self.logger.error(f"Timeout Exception thrown")
             return False 
