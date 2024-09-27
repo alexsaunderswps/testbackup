c511e641926d7ba5666f968879ff0297ecbb1ca4
Alex Saunders <alex.saunders@wildlifeprotectionsolutions.org>
2024-09-27 15:48:16 -0600

    cleared pseudocode

--- pseudocode.py
+++ pseudocode.py
@@ -1,7 +1,2 @@
-if browser_name == "chrome":
-        options = ChromeOptions()
-        if headless:
-            options.add_argument("--headless")
-        if private:
-            options.add_argument("--incognito")
-        driver = webdriver.Chrome(options=options)
+
+    
