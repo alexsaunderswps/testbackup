5ec83e765f55447d9df69ada330d1938e36044d1
Alex Saunders <alex.saunders@wildlifeprotectionsolutions.org>
2024-10-17 13:46:51 -0600

    moved tests not connected directly to connectivity testing out of test_api_connection.py

--- tests/api/test_api_connection.py
+++ tests/api/test_api_connection.py
@@ -44,35 +44,3 @@ class TestAPIConnection:
         assert response.status_code == 200, f"Failed to get response from {endpoint}. Status code: {response.status_code}"
         assert response_time < threshold, f"Response time is too high: {response_time}"
         logger.info(f"{endpoint} response time: {response_time:.3f} seconds")
-
-    @pytest.mark.api
-    @pytest.mark.connection
-    def test_get_video_by_id(self, random_video_data):
-        video_id = random_video_data["ID"]
-        expected_video_name = random_video_data["Name"]
-        expected_video_overview = random_video_data["Overview"]
-        
-        response = self.api.get(f"/Videos/{video_id}/Details")
-        response_time = self.api.measure_response_time(response)
-        
-        logger.info('-' * 80)
-        logger.info(f"Testing Video ID: {video_id}")
-        logger.info(f"Response time: {response_time}")
-        logger.info('-' * 80)
-        assert response.status_code == 200, f"Failed to get video by ID. Status code: {response.status_code}"
-        assert response_time < 0.5, f"Response time is too high: {response_time}"
-        
-        content_type = response.headers.get("Content-Type")
-        assert 'application/json' in content_type, f"Content-Type is not application/json. Content-Type: {content_type}"
-        
-        try:
-            json_response = response.json()
-            assert json_response["videoId"] == video_id
-            logger.info(f"Video ID: {json_response['videoId']}")
-            assert json_response["name"] == expected_video_name
-            logger.info(f"Video Name: {json_response['name']}")
-            assert json_response["overview"] == expected_video_overview
-            logger.info(f"Video Overview: {json_response['overview']}")
-        except requests.exceptions.JSONDecodeError:
-            assert False, "Response is not in JSON format"
-    
