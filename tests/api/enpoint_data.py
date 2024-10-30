42bb7865e9a62ef9acbdf66a3ef10ca00420e0bc
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2024-10-30 15:38:11 -0400

    Introduce action for testing on remote signal

--- tests/api/enpoint_data.py
+++ tests/api/enpoint_data.py
@@ -1,8 +1,8 @@
-#endpoints_data.py
+# endpoints_data.py
 import math
 
 class ENDPOINT_DATA:
-    
+
     ENDPOINTS = [
         "/Videos", "/VideoCatalogue", "/MapMarker", "/Countries", "/IUCNStatus",
         "/Organization", "/PopulationTrend", "/Species", "/Site", "/SpeciesCategory",
@@ -10,20 +10,20 @@ class ENDPOINT_DATA:
     ]
 
     THRESHOLDS = {
-        "/Videos": 0.4,
-        "/VideoCatalogue": 0.3,
-        "/MapMarker": 0.3,
-        "/Countries": 0.3,
-        "/IUCNStatus": 0.3,
-        "/Organization": 0.3,
-        "/PopulationTrend": 0.3,
-        "/Species": 0.3,
-        "/Site": 0.3,
-        "/SpeciesCategory": 0.3,
-        "/Tag": 0.3,
-        "/Users": 0.3,
+        "/Videos": 2,
+        "/VideoCatalogue": 2,
+        "/MapMarker": 2,
+        "/Countries": 2,
+        "/IUCNStatus": 2,
+        "/Organization": 2,
+        "/PopulationTrend": 2,
+        "/Species": 2,
+        "/Site": 2,
+        "/SpeciesCategory": 2,
+        "/Tag": 2,
+        "/Users": 2,
     }
-    
+
     # Video collection size data
     TOTAL_VIDEOS = 296
     MAX_PAGE_SIZE = 25
