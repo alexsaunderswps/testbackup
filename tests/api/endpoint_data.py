2e5706b29c23699941cef1868b463de161424ccd
alexsaunderswps <alex.saunders@wildlifeprotectionsolutions.org>
2024-10-30 11:12:51 -0600

    corrected spelling mistake in file name

---
+++ tests/api/endpoint_data.py
@@ -0,0 +1,30 @@
+#endpoints_data.py
+import math
+
+class ENDPOINT_DATA:
+    
+    ENDPOINTS = [
+        "/Videos", "/VideoCatalogue", "/MapMarker", "/Countries", "/IUCNStatus",
+        "/Organization", "/PopulationTrend", "/Species", "/Site", "/SpeciesCategory",
+        "/Tag", "/Users", "/Organization"
+    ]
+
+    THRESHOLDS = {
+        "/Videos": 0.4,
+        "/VideoCatalogue": 0.3,
+        "/MapMarker": 0.3,
+        "/Countries": 0.3,
+        "/IUCNStatus": 0.3,
+        "/Organization": 0.3,
+        "/PopulationTrend": 0.3,
+        "/Species": 0.3,
+        "/Site": 0.3,
+        "/SpeciesCategory": 0.3,
+        "/Tag": 0.3,
+        "/Users": 0.3,
+    }
+    
+    # Video collection size data
+    TOTAL_VIDEOS = 299
+    MAX_PAGE_SIZE = 25
+    TOTAL_PAGES = math.ceil(TOTAL_VIDEOS / MAX_PAGE_SIZE)
