7915e110c00ad9ed38bc72f5b770edaaddc147c7
alexsaunderswps <alex.saunders@wildlifeprotectionsolutions.org>
2025-03-20 17:45:55 -0600

    add centralized tests for admin and definitions nav elements to test_base_page_ui.py

--- tests/ui/test_base_page_ui.py
+++ tests/ui/test_base_page_ui.py
@@ -31,3 +31,47 @@ class TestBasePageUI:
                 all_browsers_passed = False
         logger.info("test_page_nav_elements: finished")
         assert all_browsers_passed, "One or more browsers failed to find all nav elements"
+        
+    @pytest.mark.UI 
+    @pytest.mark.debug
+    def test_page_admin_elements(self, base_page):
+        """_summary_
+
+        Args:
+            page_element (_type_): _description_
+        """
+        logger.debug(f"Starting test_base_page_admin_elements for {base_page[0].__class__.__name__}")
+        all_browsers_passed = True
+        for index, bp in enumerate(base_page):
+            logger.info(f"Testing admin elements on brwoser {index + 1}: {bp.driver.name}")
+            success, missing = bp.verify_all_admin_links_present()
+
+            if success:
+                logger.info(f"Verification Successful :: All Admin Elements found on {bp.driver.name}")
+            else:
+                logger.error(f"Verification Failed :: Missing Admin Elements: {', '.join(missing)} for {bp.driver.name}")
+                all_browsers_passed = False
+        logger.info("test_page_admin_elements: finished")
+        assert all_browsers_passed, "One or more browsers failed to find all admin elements"
+        
+    @pytest.mark.UI 
+    @pytest.mark.debug
+    def test_page_definition_elements(self, base_page):
+        """_summary_
+
+        Args:
+            page_element (_type_): _description_
+        """
+        logger.debug(f"Starting test_base_page_definitions_elements for {base_page[0].__class__.__name__}")
+        all_browsers_passed = True
+        for index, bp in enumerate(base_page):
+            logger.info(f"Testing admin elements on brwoser {index + 1}: {bp.driver.name}")
+            success, missing = bp.verify_all_definition_links_present()
+
+            if success:
+                logger.info(f"Verification Successful :: All Admin Elements found on {bp.driver.name}")
+            else:
+                logger.error(f"Verification Failed :: Missing Admin Elements: {', '.join(missing)} for {bp.driver.name}")
+                all_browsers_passed = False
+        logger.info("test_page_admin_elements: finished")
+        assert all_browsers_passed, "One or more browsers failed to find all admin elements"
