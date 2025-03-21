6d194c1994d9c4263ad17ef1bab75b16903e46e4
alexsaunderswps <alex.saunders@wildlifeprotectionsolutions.org>
2025-03-21 13:53:25 -0600

    added pagination test logic to test_base_page_ui.py to centralize test logic for common tests

--- tests/ui/test_base_page_ui.py
+++ tests/ui/test_base_page_ui.py
@@ -22,7 +22,7 @@ class TestBasePageUI:
         logger.debug(f"Starting test_base_page_nav_elements for {base_page[0].__class__.__name__}")
         all_browsers_passed = True
         for index, bp in enumerate(base_page):
-            logger.info(f"Testing nav elements on brwoser {index + 1}: {bp.driver.name}")
+            logger.info(f"Testing nav elements on browser {index + 1}: {bp.driver.name}")
             success, missing = bp.verify_all_nav_elements_present()
 
             if success:
@@ -45,7 +45,7 @@ class TestBasePageUI:
         logger.debug(f"Starting test_base_page_admin_elements for {base_page[0].__class__.__name__}")
         all_browsers_passed = True
         for index, bp in enumerate(base_page):
-            logger.info(f"Testing admin elements on brwoser {index + 1}: {bp.driver.name}")
+            logger.info(f"Testing admin elements on browser {index + 1}: {bp.driver.name}")
             success, missing = bp.verify_all_admin_links_present()
 
             if success:
@@ -68,7 +68,7 @@ class TestBasePageUI:
         logger.debug(f"Starting test_base_page_definitions_elements for {base_page[0].__class__.__name__}")
         all_browsers_passed = True
         for index, bp in enumerate(base_page):
-            logger.info(f"Testing admin elements on brwoser {index + 1}: {bp.driver.name}")
+            logger.info(f"Testing admin elements on browser {index + 1}: {bp.driver.name}")
             success, missing = bp.verify_all_definition_links_present()
 
             if success:
@@ -78,3 +78,26 @@ class TestBasePageUI:
                 all_browsers_passed = False
         logger.info("test_page_admin_elements: finished")
         assert all_browsers_passed, "One or more browsers failed to find all admin elements"
+        
+    @pytest.mark.UI 
+    @pytest.mark.base
+    @pytest.mark.debug
+    def test_page_pagination_elements(self,base_page):
+        """_summary_
+
+        Args:
+            base_page (_type_): _description_
+        """
+        logger.debug(f"Starting test_base_page_pagination_elements for {base_page[0].__class__.__name__}")
+        all_browsers_passed = True
+        for index, bp in enumerate(base_page):
+            logger.info(f"Testing pagination elements on browser {index + 1}: {bp.driver.name}")
+            success, missing = bp.verify_pagination_elements_present()
+
+            if success:
+                logger.info(f"Verification Successful :: All Pagination Elements found on {bp.driver.name}")
+            else:
+                logger.error(f"Verification Failed :: Missing Pagination Elements: {', '.join(missing)} for {bp.driver.name}")
+                all_browsers_passed = False
+        logger.info("test_page_pagination_elements: finished")
+        assert all_browsers_passed, "One or more browsers failed to find all pagination elements"
