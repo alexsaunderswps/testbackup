f3700b372f77dfcd865b2943f0bfe6201200cc75
alexsaunderswps <alex.saunders@wildlifeprotectionsolutions.org>
2025-05-22 09:29:26 -0600

    removed fixture import where not needed.

--- page_objects/admin_menu/installations_page.py
+++ page_objects/admin_menu/installations_page.py
@@ -3,7 +3,6 @@ import os
 from dotenv import load_dotenv
 from typing import Tuple, List
 from page_objects.common.base_page import BasePage
-from fixtures.admin_menu.installations_fixtures import installations_page
 from utilities.utils import logger
 
 load_dotenv()
